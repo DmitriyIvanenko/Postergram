6f78abed0659624a1dcb769da381781108855127
Dmytro Ivanenko <ivanenko2010@gmail.com>
2022-11-28 11:22:35 +0300

    Initial Commit

---
+++ Postergram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Postergram
+//
+//  Created by Dmytro Ivanenko on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
