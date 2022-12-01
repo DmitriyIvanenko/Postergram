86b7aedae2f58fb05bf9f7e2d186df1c99371908
Dmytro Ivanenko <ivanenko2010@gmail.com>
2022-12-01 23:26:07 +0300

    Add logo, BG, Btn settings for Login
    1. Layout of Logo image
    2. Layout of BG image
    3. Styles for Buttons

--- Postergram/Controllers/Other/Onboarding/LoginViewController.swift
+++ Postergram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -9,18 +9,47 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    struct Constants {
+        static let cornerRadius: CGFloat = 8.0
+    }
+    
     private let userNameEmailField: UITextField = {
-        return UITextField()
+        let field = UITextField()
+        field.placeholder = "Username or Email"
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        return field
     }()
     
     private let passwordField: UITextField = {
         let field = UITextField()
         field.isSecureTextEntry = true
+        field.placeholder = "Password"
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
         return field
     }()
     
     private let loginButton: UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = Constants.cornerRadius
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        return button
     }()
     
     private let termsButton: UIButton = {
@@ -32,16 +61,23 @@ class LoginViewController: UIViewController {
     }()
     
     private let createAccountButton: UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("Create Account", for: .normal)
+        return button
     }()
 
     private let headerView: UIView = {
-        return UIView()
+        let header =  UIView()
+        header.clipsToBounds = true
+        let backgroundImageView = UIImageView(image: UIImage(named: "gradient"))
+        header.addSubview(backgroundImageView)
+        return header
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addSubviews()
+        addSubViews()
         view.backgroundColor = .systemBackground
     }
     
@@ -49,13 +85,38 @@ class LoginViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
         //Asign frames
+        headerView.frame = CGRect(
+            x: 0,
+            y: 0.0,
+            width: view.width,
+            height: view.height/3.0
+        )
+        configureHeaderView()
+    }
+    
+    private func configureHeaderView() {
+        guard headerView.subviews.count == 1 else { return }
+        
+        guard let backgroundView = headerView.subviews.first else { return }
+        backgroundView.frame = headerView.bounds
+        
+        //Add image logo
+        let imageView = UIImageView(image: UIImage(named: "text"))
+        headerView.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(
+            x: headerView.width/4.0,
+            y: view.safeAreaInsets.top,
+            width: headerView.width/2.0,
+            height: headerView.height - view.safeAreaInsets.top
+        )
     }
     
     private func addSubviews() {
         view.addSubview(userNameEmailField)
     }
     
-    private func addSubviws() {
+    private func addSubViews() {
         view.addSubview(userNameEmailField)
         view.addSubview(passwordField)
         view.addSubview(loginButton)
---
+++ Postergram/Resourses/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  Postergram
+//
+//  Created by Dmytro Ivanenko on 01.12.2022.
+//
+
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var bottom: CGFloat {
+        return frame.size.width
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+    
+}
