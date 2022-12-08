cb4809573405ab6f06e319ccfbce38e4fa640906
Dmytro Ivanenko <ivanenko2010@gmail.com>
2022-12-08 19:28:49 +0300

    ProfileVC, Models, Storage, Contents, PhotoCollectionViewCell, ProfileInfoHeader, ProfileTab
    ProfileVC,
    Models,
    Storage,
    Contents,
    PhotoCollectionViewCell,
    ProfileInfoHeader,
    ProfileTab
    
    SDWebImage - Pacage added

---
+++ Postergram/Models.swift
@@ -0,0 +1,64 @@
+//
+//  Models.swift
+//  Postergram
+//
+//  Created by Dmytro Ivanenko on 08.12.2022.
+//
+
+import Foundation
+
+public enum Gender {
+    case male, female, other
+}
+
+struct User {
+    let username: String
+    let bio: String
+    let name: (first: String, last: String)
+    let birthDate: Date
+    let gender: Gender
+    let userCount: Count
+    let joinDate: Date
+    
+}
+
+struct Count {
+    let followers: Int
+    let following: Int
+    let posts: Int
+}
+
+public enum UserPostType {
+    case photo, video
+}
+
+public struct UserPost {
+    let identifier: String
+    let postType: UserPostType
+    let thumbnailImage: URL
+    let postURL: URL // either time
+    let caption: String?
+    let likeCount: [PostLike]
+    let comments: [PostComment]
+    let createdDate: Date
+    let taggedUsers: [User]
+}
+
+struct PostLike {
+    let username: String
+    let postidentifier: String
+}
+
+struct CommentLike {
+    let username: String
+    let commentIdentifier: String
+}
+
+struct PostComment {
+    let identifier: String
+    let username: String
+    let text: String
+    let createdDate: Date
+    let likes: [CommentLike]
+}
+
